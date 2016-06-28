aaac08feb27a911fbec479fb2fb26aa95925c868
devill <devillmeister@gmail.com>
2016-06-28 10:44:39 +0200

    Only all in on pairs

--- player.rb
+++ player.rb
@@ -4,7 +4,14 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    10000
+    me = game_state['players'][game_state['in_action']]
+    hole_cards = me['hole_cards']
+
+    if hole_cards[0]['rank'] == hole_cards[1]['rank']
+      10000
+    else
+      0
+    end
   end
 
   def showdown(game_state)
