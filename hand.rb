c3142fdab78ff459e57c1ec6474b3d2aadf35cf6
devill <devillmeister@gmail.com>
2016-06-28 11:53:44 +0200

    Less agressive

--- hand.rb
+++ hand.rb
@@ -11,6 +11,22 @@ class Hand
   end
 
   def high_pocket_pair?
-    pocket_pair? && @hole_cards[0].value >= 10
+    pocket_pair? && highest_pocket_card_value >= 10
+  end
+
+  def highest_pocket_card_value
+    @hole_cards.map { |card| card.value }.max
+  end
+
+  def pocket_connector?
+    (@hole_cards[0].value - @hole_cards[1].value).abs == 1
+  end
+
+  def suited_pocket?
+    @hole_cards[0].suit == @hole_cards[1].suit
+  end
+
+  def high_suited_pocket_connector?
+    pocket_connector? && suited_pocket? && highest_pocket_card_value >= 13
   end
 end
--- player.rb
+++ player.rb
@@ -5,11 +5,18 @@ class Player
   VERSION = "Get a bit less agressive"
 
   def bet_request(game_state)
+    [0,bet(game_state).to_i].max
+  rescue Excpetion => e
+    STDERR.p e.message
+    0
+  end
+
+  def bet(game_state)
     me = game_state['players'][game_state['in_action']]
 
     hand = Hand.new(me['hole_cards'], game_state['community_cards'])
 
-    if hand.high_pocket_pair?
+    if hand.high_pocket_pair? || hand.high_suited_pocket_connector?
       10000
     else
       0
