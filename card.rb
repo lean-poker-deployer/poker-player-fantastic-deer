da41943a16913485d77d728c8ec9c3f057186744
devill <devillmeister@gmail.com>
2016-06-28 11:05:24 +0200

    Refactor

--- card.rb
+++ card.rb
@@ -1,11 +1,15 @@
 
 
 class Card
+  attr_reader :rank
+  attr_reader :suit
+  attr_reader :value
+
   def initialize(card)
     high_values = { 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 }
 
     @rank = card['rank']
     @suit = card['suit']
-    @value = @rank.to_i
+    @value = high_values.has_key?(@rank) ? high_values[@rank] : @rank.to_i
   end
 end
--- hand.rb
+++ hand.rb
@@ -1,9 +1,12 @@
+require_relative 'card'
 
 class Hand
   def initialize(hole_cards, community_cards)
-    @hole_cards = hole_cards
-    @community_cards = community_cards
+    @hole_cards = hole_cards.map { |card| Card.new(card) }
+    @community_cards = community_cards.map { |card| Card.new(card) }
   end
 
-
+  def pocket_pair?
+    @hole_cards[0].value == @hole_cards[1].value
+  end
 end
